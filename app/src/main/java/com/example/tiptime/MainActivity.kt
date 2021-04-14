49264ee19f4b4b685ce4b41c819f17e1e0bf8bef
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2021-04-14 09:28:12 -0300

    pequenas mudanças no  XML da aplicação e conclusão dos cálculos da gorjeta

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,8 +1,9 @@
-jetapackage com.example.tiptime
+package com.example.tiptime
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +18,19 @@ class MainActivity : AppCompatActivity() {
     fun calculateTip(){
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
+        val selectId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when(selectId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+        val roundUp = binding.roundUpTip.isChecked
+
+        if(roundUp) tip = kotlin.math.ceil(tip)
+
+        val formatedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formatedTip)
 
     }
 }
